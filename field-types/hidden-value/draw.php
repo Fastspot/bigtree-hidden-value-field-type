cbf298b1236e2a060f700fe753d0ef81046237cc
Jemal Cole <Jemal.Cole@centretek.com>
2016-06-30 14:23:14 -0400

    Draw a hidden field on the form so that we can use it in processing hooks

--- field-types/hidden-value/draw.php
+++ field-types/hidden-value/draw.php
@@ -1,3 +1,5 @@
-<?
-	// We draw nothing!
+
+<?php
+	// Draw the field as a hidden field so that it's available for processing hooks
 ?>
+<input type="hidden" name="<?=$field["key"]?>" value="<?=$field["value"]?>" id="<?=$field["id"]?>" />
